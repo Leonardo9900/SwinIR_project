a927f95023ba276771607b0f9f4d6eccea58d2de
Leonardo Mongelli <73338494+Leonardo9900@users.noreply.github.com>
2022-08-19 17:40:17 +0200

    Update main_test_swinir.py
    dot deleted in line 222 and 223

--- main_test_swinir.py
+++ main_test_swinir.py
@@ -219,9 +219,9 @@ def get_image_pair(args, path):
 
     # 001 classical image sr/ 002 lightweight image sr (load lq-gt image pairs)
     if args.task in ['classical_sr', 'lightweight_sr']:
-        img_gt = cv2.imread(path, cv2.IMREAD_COLOR).astype(np.float32) / 255.
+        img_gt = cv2.imread(path, cv2.IMREAD_COLOR).astype(np.float32) / 255
         img_lq = cv2.imread(f'{args.folder_lq}/{imgname}x{args.scale}{imgext}', cv2.IMREAD_COLOR).astype(
-            np.float32) / 255.
+            np.float32) / 255
 
     # 003 real-world image sr (load lq image only)
     elif args.task in ['real_sr']:
