2efa03fedf0eb203e7f59756983e6afb90185f47
Leonardo Mongelli <73338494+Leonardo9900@users.noreply.github.com>
2022-08-19 16:13:20 +0200

    Update utils_image.py

--- utils/utils_image.py
+++ utils/utils_image.py
@@ -627,10 +627,11 @@ def calculate_psnr(img1, img2, border=0):
     # img1 and img2 have range [0, 255]
     #img1 = img1.squeeze()
     #img2 = img2.squeeze()
-    if not img1.shape == img2.shape:
-        print(img1.shape)   
-        print(img2.shape)   
-        raise ValueError('Input images must have the same dimensions.')
+    try:
+        not img1.shape == img2.shape
+    except ValueError:
+        print('Input images must have the same dimensions.', img1.shape, img2.shape)
+    
     h, w = img1.shape[:2]
     img1 = img1[border:h-border, border:w-border]
     img2 = img2[border:h-border, border:w-border]
